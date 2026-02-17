f3dc9c7a905d0a6c64d89413c4f3a2e59b43edd7
oleh <phd.chornyi@gmail.com>
2026-02-17 14:21:12 +0200

    investment calculator(3)

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,16 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+// func main() {
+// 	const inflationRate = 2.5
+// 	var investmentAmount float64
+// 	years, expectedReturnRate := 10.0, 5.5
+
+// 	fmt.Print("Investment Amount: ")
+// 	fmt.Scan(&investmentAmount)
+
+// 	fmt.Print("Expected Return Rate: ")
+// 	fmt.Scan(&expectedReturnRate)
+
+// 	fmt.Print("Investment Horizon(years): ")
+// 	fmt.Scan(&years)
+
+// 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+// 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+// 	fmt.Println(futureValue)
+// 	fmt.Println(futureRealValue)
+// }
 
 func main() {
-	const inflationRate = 2.5
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
 }

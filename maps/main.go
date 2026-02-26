2540b27d190775b6e61b2a05d8d774e50f07fea8
oleh <phd.chornyi@gmail.com>
2026-02-26 15:39:55 +0200

    make for maps and slices(arrays)

--- maps/main.go
+++ maps/main.go
@@ -2,25 +2,44 @@ package main
 
 import "fmt"
 
-type Websites struct {
-	google string
-	aws    string
-}
-
 func main() {
-	fmt.Println("Maps things")
-	websites := map[string]string{
-		"Google":              "https://google.com",
-		"Amazon Web Services": "https://aws.com",
-	}
+	//1st - length, 2nd - capacity
+	userNames := make([]string, 2, 5)
 
-	fmt.Println(websites)
-	fmt.Println(websites["Amazon Web Services"])
+	userNames[0] = "Serhiy"
+	userNames[1] = "Yevhen"
 
-	websites["LinkedIn"] = "https://linkedin.com"
-	fmt.Println(websites)
+	userNames = append(userNames, "Oleh")
+	userNames = append(userNames, "Ihor")
 
-	delete(websites, "Google")
-	fmt.Println(websites)
+	fmt.Println(userNames)
 
+	cources := make(map[string]float64, 3)
+
+	cources["go"] = 4.7
+	cources["flutter"] = 4.8
+
+	fmt.Println(cources)
 }
+
+// type Websites struct {
+// 	google string
+// 	aws    string
+// }
+
+// func main() {
+// 	fmt.Println("Maps things")
+// 	websites := map[string]string{
+// 		"Google":              "https://google.com",
+// 		"Amazon Web Services": "https://aws.com",
+// 	}
+
+// 	fmt.Println(websites)
+// 	fmt.Println(websites["Amazon Web Services"])
+
+// 	websites["LinkedIn"] = "https://linkedin.com"
+// 	fmt.Println(websites)
+
+// 	delete(websites, "Google")
+// 	fmt.Println(websites)
+// }

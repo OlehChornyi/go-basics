8efcb19ed6f617227cc2ba39895347a20f22fc36
oleh <phd.chornyi@gmail.com>
2026-02-27 10:55:42 +0200

    functions deep dive(3)

--- functions_deep/main.go
+++ functions_deep/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+type transformFn func(int) int
+
 // Functions as firs-class values (pass functions)
 func main() {
 	numbers := []int{1, 2, 3, 4}
@@ -12,15 +14,34 @@ func main() {
 
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(tripled)
+
+	moreNumbers := []int{5, 1, 2}
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
+func transformNumbers(numbers *[]int, transform transformFn) []int {
 	dNumbers := []int{}
 	for _, value := range *numbers {
 		dNumbers = append(dNumbers, transform(value))
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {

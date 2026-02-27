30633cbaafbb8ae5ae7ad369d11f6c90537e283b
oleh <phd.chornyi@gmail.com>
2026-02-27 10:22:53 +0200

    functions deep dive(2)

--- functions_deep/main.go
+++ functions_deep/main.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
+// Functions as firs-class values (pass functions)
 func main() {
 	numbers := []int{1, 2, 3, 4}
 	fmt.Println(numbers)
 
-	doubled := doubleNumbers(&numbers)
+	doubled := transformNumbers(&numbers, double)
 	fmt.Println(doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(tripled)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 	for _, value := range *numbers {
-		dNumbers = append(dNumbers, double(value))
+		dNumbers = append(dNumbers, transform(value))
 	}
 
 	return dNumbers
@@ -21,4 +25,8 @@ func doubleNumbers(numbers *[]int) []int {
 
 func double(number int) int {
 	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }

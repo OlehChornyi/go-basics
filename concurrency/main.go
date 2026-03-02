9751e1cd82db75248c44fcc62ed95a600db89d38
oleh <phd.chornyi@gmail.com>
2026-03-02 18:56:25 +0200

    concurrency(2)(channels)

--- concurrency/main.go
+++ concurrency/main.go
@@ -14,18 +14,31 @@ func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
+	// dones := make([]chan bool, 4)
 	done := make(chan bool)
 
+	// dones[0] = make(chan bool)
 	go greet("Hi!", done)
+	// dones[1] = make(chan bool)
 	go greet("You!", done)
+	// dones[2] = make(chan bool)
 	go slowGreet("Blah ... blah ... blah", done)
+	// dones[3] = make(chan bool)
 	go greet("Have a nice day!", done)
 
-	<-done
-	<-done
-	<-done
-	<-done
+	// for _, done := range dones {
+	// 	<-done
+	// }
+
+	// for doneChan := range done {
+	// 	fmt.Println(doneChan)
+	// }
+
+	for range done {
+		// 	fmt.Println(doneChan)
+	}
 }

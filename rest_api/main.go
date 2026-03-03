6f820bf947a5230a953787c6c571981f7bc9e0c3
oleh <phd.chornyi@gmail.com>
2026-03-03 12:56:50 +0200

    restAPI(fist POST + restClient vsCode extension)

--- rest_api/main.go
+++ rest_api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest_api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,10 +12,29 @@ func main() {
 
 	//Register a handler for incoming http request
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
